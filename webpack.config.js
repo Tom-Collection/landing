368473e1a4e60372ec5f14a930a5db19f3966ee6
Artem <artem.marshalkin@icloud.com>
2023-12-08 18:28:40 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const { GenerateSW} = require('workbox-webpack-plugin')
+
+module.exports = {
+    plugins: [
+        new GenerateSW({
+            swSrc: "src/service-worker.js", // custom sw rule
+            swDest: "build/service-worker.js", // sw output file (auto-generated
+            globDirectory: "build",
+            globPatterns: ["**/*.{js,css,html,png,svg}"],
+            maximumFileSizeToCacheInBytes: 5 * 1024 * 102,
+        })
+    ]
+}
